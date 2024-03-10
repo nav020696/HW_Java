1413218e5b14afca44d1e2ea8ce62af371727c80
anna_neumoina <nav020@bk.ru>
2024-03-10 15:54:21 +0400

    Добавлен выходной файл output.txt с решением

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         Reader reader = new Reader(new HashMap());
-        reader.run("src/input.txt");
+        reader.run("input.txt");
     }
 }
